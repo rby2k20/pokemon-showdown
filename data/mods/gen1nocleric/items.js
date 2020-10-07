b5dbfab631204250df00d52e848bcbd15e6f56cb
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-10-07 04:50:01 +0100

    Create Gen 1 No Cleric Clause Mod
    Features 4 custom items that are basically Berserk Genes that add major status.

---
+++ data/mods/gen1nocleric/items.js
@@ -0,0 +1,66 @@
+export const BattleItems: {[k: string]: ModdedItemData} = {
+	"frz": {
+		id: "frz",
+		name: "FRZ",
+		spritenum: 305,
+		onUpdate(pokemon) {
+			onResidualOrder: 26,
+			onResidualSubOrder: 2,
+			onResidual(pokemon) {
+			pokemon.trySetStatus('frz', pokemon);
+		},
+		},
+		num: 0,
+		gen: 1,
+		isNonstandard: "Past",
+		desc: "(Gen 1) On switch-in, set FRZ. For use in Non-Cleric Clause.",
+	},
+	"psn": {
+		id: "psn",
+		name: "PSN",
+		spritenum: 515,
+		onUpdate(pokemon) {
+			onResidualOrder: 26,
+			onResidualSubOrder: 2,
+			onResidual(pokemon) {
+			pokemon.trySetStatus('psn', pokemon);
+		},
+		},
+		num: 0,
+		gen: 1,
+		isNonstandard: "Past",
+		desc: "(Gen 1) On switch-in, set PSN. For use in Non-Cleric Clause.",
+	},
+	"brn": {
+		id: "brn",
+		name: "BRN",
+		spritenum: 145,
+		onUpdate(pokemon) {
+			onResidualOrder: 26,
+			onResidualSubOrder: 2,
+			onResidual(pokemon) {
+			pokemon.trySetStatus('brn', pokemon);
+		},
+		},
+		num: 0,
+		gen: 1,
+		isNonstandard: "Past",
+		desc: "(Gen 1) On switch-in, set BRN. For use in Non-Cleric Clause.",
+	},
+	"prz": {
+		id: "prz",
+		name: "PRZ",
+		spritenum: 273,
+		onUpdate(pokemon) {
+			onResidualOrder: 26,
+			onResidualSubOrder: 2,
+			onResidual(pokemon) {
+			pokemon.trySetStatus('prz', pokemon);
+		},
+		},
+		num: 0,
+		gen: 1,
+		isNonstandard: "Past",
+		desc: "(Gen 1) On switch-in, set PRZ. For use in Non-Cleric Clause.",
+	},
+};
