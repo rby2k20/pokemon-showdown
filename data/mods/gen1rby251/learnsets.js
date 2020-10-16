e9b56f175878111a80b569715b17bad3f2913fa4
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-10-16 10:39:26 +0100

    Add Gen 1 Learnset Placeholders

--- data/mods/gen1rby251/learnsets.js
+++ data/mods/gen1rby251/learnsets.js
@@ -2,36 +2,767 @@
 
 /**@type {{[k: string]: LearnsetData}} */
 let BattleLearnsets = {
-	missingno: {
+	missingno: { //Gen 1 Pokemon here, doing it because some stuff wasn't working before. Plus, gives me opportunity to edit more later if necessary.
 		learnset: {
-			blizzard: ["5L1"],
-			bubblebeam: ["5L1"],
-			cut: ["5L1"],
-			doubleedge: ["5L1"],
-			earthquake: ["5L1"],
-			fissure: ["5L1"],
-			fly: ["5L1"],
-			icebeam: ["5L1"],
-			megakick: ["5L1"],
-			megapunch: ["5L1"],
-			psychic: ["5L1"],
-			rage: ["5L1"],
-			razorwind: ["5L1"],
-			rest: ["5L1"],
-			seismictoss: ["5L1"],
-			skyattack: ["5L1"],
-			submission: ["5L1"],
-			swordsdance: ["5L1"],
-			takedown: ["5L1"],
-			teleport: ["5L1"],
-			thunder: ["5L1"],
-			thunderwave: ["5L1"],
-			toxic: ["5L1"],
-			triattack: ["5L1"],
-			watergun: ["5L1"],
+			inherit: true,
 		},
 	},
-	chikorita: {
+	bulbasaur: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	ivysaur: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	venusaur: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	charmander: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	charmeleon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	charizard: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	squirtle: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	wartortle: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	blastoise: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	caterpie: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	metapod: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	butterfree: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	weedle: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kakuna: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	beedrill: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	pidgey: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	pidgeotto: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	pidgeot: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	rattata: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	raticate: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	spearow: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	fearow: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	ekans: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	arbok: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	pikachu: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	raichu: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	sandshrew: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	sandslash: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidoranf: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidorina: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidoqueen: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidoranm: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidorino: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	nidoking: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	clefairy: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	clefable: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	vulpix: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	ninetales: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	jigglypuff: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	wigglytuff: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	zubat: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	golbat: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	oddish: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	gloom: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	vileplume: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	paras: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	parasect: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	venonat: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	venomoth: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	diglett: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dugtrio: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	meowth: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	persian: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	psyduck: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	golduck: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	mankey: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	primeape: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	growlithe: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	arcanine: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	poliwag: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	poliwhirl: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	poliwrath: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	abra: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kadabra: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	alakazam: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	machop: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	machoke: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	machamp: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	bellsprout: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	weepinbell: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	victreebel: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	tentacool: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	tentacruel: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	geodude: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	graveler: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	golem: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	ponyta: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	rapidash: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	slowpoke: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	slowbro: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	magnemite: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	magneton: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	farfetchd: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	doduo: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dodrio: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	seel: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dewgong: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	grimer: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	muk: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	shellder: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	cloyster: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	gastly: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	haunter: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	gengar: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	onix: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	drowzee: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	hypno: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	krabby: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kingler: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	voltorb: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	electrode: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	exeggcute: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	exeggutor: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	cubone: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	marowak: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	hitmonlee: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	hitmonchan: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	lickitung: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	koffing: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	weezing: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	rhyhorn: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	rhydon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	chansey: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	tangela: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kangaskhan: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	horsea: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	seadra: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	goldeen: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	seaking: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	staryu: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	starmie: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	mrmime: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	scyther: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	jynx: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	electabuzz: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	magmar: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	pinsir: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	tauros: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	magikarp: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	gyarados: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	lapras: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	ditto: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	eevee: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	vaporeon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	jolteon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	flareon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	porygon: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	omanyte: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	omastar: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kabuto: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	kabutops: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	aerodactyl: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	snorlax: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	articuno: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	zapdos: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	moltres: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dratini: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dragonair: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	dragonite: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	mewtwo: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	mew: {
+		learnset: {
+			inherit: true,
+		},
+	},
+	chikorita: { //Gen 2 Pokemon start here.
 		learnset: {
 			takedown: ["5L1"], 
 			doubleedge: ["5L1"], 
@@ -119,6 +850,20 @@ let BattleLearnsets = {
 			earthquake: ["5L1"],
 		},
 	},
+	cyndaquil: {
+		learnset: {
+			takedown: ["5L1"], 
+			doubleedge: ["5L1"], 
+			toxic: ["5L1"], 
+			rage: ["5L1"], 
+			mimic: ["5L1"], 
+			doubleteam: ["5L1"], 
+			bide: ["5L1"], 
+			rest: ["5L1"], 
+			substitute: ["5L1"],
+		},
+	},
+},
 };
 
 exports.BattleLearnsets = BattleLearnsets;
