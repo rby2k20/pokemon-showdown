12e167f5e1880f01861710c73521e31106260e11
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-10-19 04:21:27 +0100

    Add The Rhydon Stakes
    For the culture.

---
+++ data/mods/gen1rhydon/formats-data.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+	rhydon: {
+		randomBattleMoves: ["pound"],
+		tier: "OU",
+	},
+};
+
+exports.BattleFormatsData = BattleFormatsData;
---
+++ data/mods/gen1rhydon/pokedex.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.BattlePokedex = {
+	rhydon: {
+		num: 112,
+		species: "Rhydon",
+		types: ["Ground", "Rock"],
+		gender: "N",
+		baseStats: {hp: 105, atk: 130, def: 120, spa: 45, spd: 45, spe: 40},
+		abilities: {},
+		heightm: 1.9,
+		weightkg: 120,
+		color: "Gray",
+		prevo: "rhyhorn",
+		evos: ["rhyperior"],
+		evoLevel: 42,
+		eggGroups: ["Monster", "Field"],
+	},
+};
