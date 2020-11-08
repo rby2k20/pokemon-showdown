f7b8c43cde4af2faca4d0491ad5055d86ef7bddf
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-08 17:05:28 +0000

    Update items.js

--- data/mods/gen2spaceworld97/items.js
+++ data/mods/gen2spaceworld97/items.js
@@ -35,11 +35,9 @@ let BattleItems = {
 		desc: "Weakens Dragon-type moves.",
 		onSourceModifyDamage(damage, source, target, move) {
 			if (move.type === 'Dragon' && target.getMoveHitData(move).typeMod > 0 && (!target.volatiles['substitute'] || move.flags['authentic'] || (move.infiltrates && this.gen >= 6))) {
-				if (target.eatItem()) {
-					this.debug('-50% reduction');
-					this.add('-enditem', target, this.effect, '[weaken]');
-					return this.chainModify(0.5);
-				}
+				this.debug('-50% reduction');
+				this.add('-enditem', target, this.effect, '[weaken]');
+				return this.chainModify(0.5);
 			}
 		},
 		isNonstandard: null,
