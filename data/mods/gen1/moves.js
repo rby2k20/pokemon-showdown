19da9c59338e3185eae0390cc2f2736cc47cc53a
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-11 23:25:20 +0000

    add zarel's psywave edit

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -689,13 +689,12 @@ let BattleMovedex = {
 		inherit: true,
 		basePower: 1,
 		damageCallback(pokemon) {
-			let psywaveDamage = (this.random(0, (1.5 * pokemon.level)));
+			const psywaveDamage = (this.random(0, this.trunc(1.5 * pokemon.level)));
 			if (psywaveDamage <= 0) {
 				this.hint("Desync Clause activated.");
 				return false;
-			} else {
-				return psywaveDamage;
 			}
+			return psywaveDamage;
 		},
 	},
 	rage: {
