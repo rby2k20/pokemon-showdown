229a68a642c3cc8cbac13245fd09affc9cffaa13
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-11 20:21:05 +0000

    Update moves.js

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -690,12 +690,12 @@ let BattleMovedex = {
 		basePower: 1,
 		damageCallback(pokemon) {
 			return (this.random(0, (1.5 * pokemon.level)));
-			onHit(target) {
-				if (damage < 0) {
-					this.hint("Desync Clause activated.");
-					damage = false;
-					return false;
-					}
+			},
+		onHit(target) {
+			if (damage < 0) {
+				this.hint("Desync Clause activated.");
+				damage = false;
+				return false;
 				}
 			}
 		},
