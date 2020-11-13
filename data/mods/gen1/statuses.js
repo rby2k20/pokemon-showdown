8f88afd80a1789ed24317bfcdbcce6db7646bd27
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-13 12:21:47 +0000

    Update statuses.js

--- data/mods/gen1/statuses.js
+++ data/mods/gen1/statuses.js
@@ -48,9 +48,9 @@ let BattleStatuses = {
 		onBeforeMovePriority: 2,
 		onBeforeMove(pokemon) {
 			if (this.randomChance(63, 256)) {
-				let desyncCheck = this.queue.willMove(pokemon); //Stores queued move to check for Counter desyncs.
-				this.hint(desyncCheck);
+				let desyncCheck = this.dex.getMove(pokemon.queue.willMove.id); //Stores queued move to check for Counter desyncs.
 				this.add('cant', pokemon, 'par');
+				this.hint(desyncCheck);
 				pokemon.removeVolatile('bide');
 				pokemon.removeVolatile('twoturnmove');
 				pokemon.removeVolatile('fly');
