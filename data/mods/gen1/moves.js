bd0f5c2c159c6fb60c397643afe78fcfe71005dc
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-13 09:25:07 +0000

    Update moves.js

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -2,8 +2,8 @@
  * A lot of Gen 1 moves have to be updated due to different mechanics.
  * Some moves have had major changes, such as Bite's typing.
  */
-var desyncCheck = null, //maybe this will work
 'use strict';
+var desyncCheck = null, //maybe this will work
 /**@type {{[k: string]: ModdedMoveData}} */
 let BattleMovedex = {
 	absorb: {
