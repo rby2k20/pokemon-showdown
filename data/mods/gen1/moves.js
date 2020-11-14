6f4f311b563654f87d539c1628e28dd5327d69a0
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-14 01:39:01 +0000

    Update moves.js

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -254,7 +254,7 @@ let BattleMovedex = {
 			// That means that, if opponent switches, counter will use last counter damage * 2.
 			// Counter has a lot of ways to desync, these need to be covered by Desync Clause.
 			let lastUsedMove = target.side.lastMove && this.dex.getMove(target.side.lastMove.id);
-			if (['Normal', 'Fighting'].includes(target.statusData.failedMoveid.id) || !['Normal', 'Fighting'].includes(target.statusData.failedMoveid.id)) {
+			if (['Normal', 'Fighting'].includes(target.statusData.failedMoveid) || !['Normal', 'Fighting'].includes(target.statusData.failedMoveid)) {
 				this.hint("Desync Clause Mod activated.");
 				return false;
 			} else if (lastUsedMove && lastUsedMove.basePower > 0 && ['Normal', 'Fighting'].includes(lastUsedMove.type) && this.lastDamage > 0 && !this.queue.willMove(target)) {
