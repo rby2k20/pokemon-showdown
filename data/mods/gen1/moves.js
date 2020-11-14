bfb7061af2622770d480cb77903d38d66178518d
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-14 00:56:29 +0000

    Update moves.js

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -257,7 +257,8 @@ let BattleMovedex = {
 			if (['Normal', 'Fighting'].includes(target.statusData.failedMoveid) && this.lastDamage <= 0 || ['!Normal', '!Fighting'].includes(target.statusData.failedMoveid) && this.lastDamage > 0) {
 				this.hint("Desync Clause Mod activated.");
 				return false;
-			} else if (lastUsedMove && lastUsedMove.basePower > 0 && ['Normal', 'Fighting'].includes(lastUsedMove.type) && this.lastDamage > 0 && !this.queue.willMove(target)) {
+			}
+			if (lastUsedMove && lastUsedMove.basePower > 0 && ['Normal', 'Fighting'].includes(lastUsedMove.type) && this.lastDamage > 0 && !this.queue.willMove(target)) {
 				return 2 * this.lastDamage; 
 			}
 			this.debug("Gen 1 Counter failed due to conditions not met");
